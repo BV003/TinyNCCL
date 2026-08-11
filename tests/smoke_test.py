bf978d5c1a4e252d07a5d4377eb133fc7079eb36
michael <michaelliuwq003@gmail.com>
2026-08-11 11:12:31 +0800

    Aug 11

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -2,6 +2,9 @@ import os
 import torch
 import torch.distributed as dist
 
+local_rank = int(os.environ["LOCAL_RANK"])
+torch.cuda.set_device(local_rank)
+
 dist.init_process_group(backend="nccl", init_method="env://")
 rank = dist.get_rank()
 t = torch.full((4,), rank + 1, device="cuda")
