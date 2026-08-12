a0570ef1d59e76ea2b226fa62d715742f0b7e0da
michael <michaelliuwq003@gmail.com>
2026-08-12 16:11:44 +0800

    Aug 12

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ I do this project for learning about NCCL and Distributed system.
 ## Hardware Requirements
 
 2* A5000
+
+## Build & Install
+
+```bash
+pip install -e .
+```
+
+## Run Tests
+
+```bash
+python tests/test_kernels.py
+```
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name="tiny",
+    ext_modules=[
+        CUDAExtension(
+            name="tiny",
+            sources=["src/kernels.cu", "src/bindings.cpp"],
+            extra_compile_args={
+                "cxx": ["-std=c++20"],
+                "nvcc": ["-O3", "-std=c++17"],
+            },
+        ),
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
